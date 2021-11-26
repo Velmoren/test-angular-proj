083c97d5c967dd4143b419a8ed24b058d12d452d
Velmoren <velmoren@yahoo.com>
2021-11-27 02:00:31 +0300

    basis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+export interface Post {
+  title: string,
+  text: string,
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-ng-project';
+
+export class AppComponent implements OnInit {
+  posts: Post[] = [
+    {title: 'Хочу выучить Angular коспоненты', text: 'Я все еще учу компоненты', id: 1},
+    {title: 'Следующий блок', text: 'Будет про директивы и еще про пайпы', id: 2},
+  ]
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post)
+  }
+
+  removePost(id: number) {
+    this.posts = this.posts.filter(p => p.id !== id)
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      console.log('timeout')
+      this.posts[0] = {
+        title: 'Changed',
+        text: 'Changed text',
+        id: 5
+      }
+    }, 5000)
+  }
 }
---
+++ src/app/post/post.component.ts
@@ -0,0 +1,53 @@
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  Component,
+  ContentChild,
+  DoCheck,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  ViewEncapsulation
+} from '@angular/core';
+import {Post} from "../app.component";
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
+})
+export class PostComponent implements AfterContentInit, OnChanges, DoCheck {
+
+  @Input() post: Post = {
+    title: '',
+    text: ''
+  }
+  @Output() onRemove = new EventEmitter<number>()
+  @ContentChild('info') infoRef!: ElementRef
+
+  removePost() {
+    this.onRemove.emit(this.post.id)
+  }
+
+  constructor() {
+    console.log('constructor')
+  }
+
+  ngAfterContentInit() {
+    console.log(this.infoRef?.nativeElement)
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes)
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck')
+  }
+
+}
