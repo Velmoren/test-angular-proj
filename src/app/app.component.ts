0a30bf6823acca12f11b42ed1c54794ff61eb61e
Aleksander <ilyasov_as@creacept.ru>
2021-11-27 11:47:43 +0300

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 export interface Post {
   title: string,
@@ -12,28 +12,6 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit {
-  posts: Post[] = [
-    {title: 'Хочу выучить Angular коспоненты', text: 'Я все еще учу компоненты', id: 1},
-    {title: 'Следующий блок', text: 'Будет про директивы и еще про пайпы', id: 2},
-  ]
-
-  updatePosts(post: Post) {
-    this.posts.unshift(post)
-  }
-
-  removePost(id: number) {
-    this.posts = this.posts.filter(p => p.id !== id)
-  }
-
-  ngOnInit(): void {
-    setTimeout(() => {
-      console.log('timeout')
-      this.posts[0] = {
-        title: 'Changed',
-        text: 'Changed text',
-        id: 5
-      }
-    }, 5000)
-  }
+export class AppComponent {
+  isVisible = true
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostFormComponent } from './post-form/post-form.component';
-import { PostComponent } from './post/post.component';
 import {FormsModule} from "@angular/forms";
+import {StyleDirective} from "./directives/style.directive";
+import { IfnotDirective } from './directives/ifnot.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostFormComponent,
-    PostComponent,
+    StyleDirective,
+    IfnotDirective,
   ],
   imports: [
     BrowserModule,
