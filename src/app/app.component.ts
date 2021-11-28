8f99145ea7c5860b93b3d89e72fd277c9b470a5e
Velmoren <velmoren@yahoo.com>
2021-11-28 12:08:15 +0300

    add validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {HttpClient} from "@angular/common/http";
+import {delay} from "rxjs";
 
-export interface Post {
-  title: string,
-  text: string,
+export interface Todo {
+  completed: boolean
+  title: string
   id?: number
 }
 
@@ -11,7 +13,50 @@ export interface Post {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+export class AppComponent implements OnInit {
 
-export class AppComponent {
+  todos: Todo[] = []
+  todoTitle: string = ''
+  loading: boolean = false
 
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    // this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2').subscribe(todos => {this.todos = todos})
+
+    this.getTodos()
+  }
+
+  getTodos() {
+    this.loading = true
+    this.http.get<Todo[]>('http://localhost:3000/todos')
+      .pipe(delay(500))
+      .subscribe(todos => {
+        this.todos = todos
+        this.loading = false
+      })
+  }
+
+  addTodos() {
+    if (!this.todoTitle.trim()) return
+
+    const newTodo: Todo = {title: this.todoTitle, completed: false}
+
+    // this.http.post('https://jsonplaceholder.typicode.com/todos', newTodo).subscribe(todo => {console.log(todo)})
+    this.http.post<Todo[]>('http://localhost:3000/todos', newTodo)
+      .subscribe(todo => {
+        this.getTodos()
+        this.todoTitle = ''
+      })
+  }
+
+  deleteTodo(id?: number) {
+    this.http.delete(`http://localhost:3000/todos/${id}`)
+      .subscribe(() => {
+        // this.todos = this.todos.filter(todo => todo.id !== id)
+        this.getTodos()
+      })
+  }
 }
+
